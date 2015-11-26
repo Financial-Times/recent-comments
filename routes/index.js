2956bf2304d168f0bdee89a4e8dba4c998584bff
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2015-11-26 12:14:01 +0200

    Add __health.json route

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/v1/recent-comments', controllers.recentComments);
 router.get('/__about', controllers.about);
 router.get('/__health', controllers.health);
+router.get('/__health.json', controllers.health);
 
 module.exports = router;
