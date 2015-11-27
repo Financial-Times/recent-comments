8f31d24a346d16571e74114544de6ea8ac51bd9a
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2015-11-27 10:54:22 +0200

    Change gtg endpoint

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const controllers = require('../controllers');
 const router = express.Router();
 
 router.get('/v1/recent-comments', controllers.recentComments);
-router.get('/v1/__gtg', controllers.gtg);
+router.get('/__gtg', controllers.gtg);
 router.get('/__about', controllers.about);
 router.get('/__health', controllers.health);
 router.get('/__health.json', controllers.health);
