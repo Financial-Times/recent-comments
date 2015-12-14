c13ba9f65bd127c748c26ade0dc8c4616e1f7b3c
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2015-12-14 17:20:59 +0200

    Add apidocs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,47 @@ const express = require('express');
 const controllers = require('../controllers');
 const router = express.Router();
 
+/**
+ * @api {get} /v1/recent-comments recent-comments
+ * @apiExample Example usage:
+ * curl -i http://recent-comments-test.herokuapp.com/v1/recent-comments?siteid=377197&tagname=sections.UK&count=1
+ * @apiVersion 1.2.2
+ * @apiGroup v1
+ * @apiDescription Get most recent comments for articles with tags specified by tagname param from the site with id of siteid ordered by createdAt field, descending.
+ *
+ * @apiParam {Number} siteid  ID of the site.
+ * @apiParam {String} tagname Article tag name.
+ * @apiParam {Number} [count] Number of comments to display (max=100).
+ *
+ * @apiSuccess {Object[]} result List of comments.
+ * @apiSuccess {String} result.commentId Id of the comment.
+ * @apiSuccess {String} result.bodyHtml Body of the comment.
+ * @apiSuccess {Number} result.createdAt Timestamp in microseconds.
+ * @apiSuccess {String} result.displayName Display name of the comment's author.
+ * @apiSuccess {String} result.articleId Id of the article.
+ * @apiSuccess {String} result.url Url of the article.
+ * @apiSuccess {String} result.title Title of the article.
+ *
+ *
+ * @apiSuccessExample Normal success
+ *  HTTP/1.1 200 OK
+ *  [
+ *      {
+ *          "commentId":"420291017",
+ *          "bodyHtml":"new test",
+ *          "createdAt":1448618340000,
+ *          "displayName":"ddragos-test",
+ *          "articleId":"944938b4-8d0c-11e5-8be4-3506bf20cc2b",
+ *          "url":"http://www.ft.com/cms/s/0/944938b4-8d0c-11e5-8be4-3506bf20cc2b.html",
+ *          "title":"UK inflation below zero for second month - FT.com"
+ *      }
+ *  ]
+ *
+ * @apiSuccessExample No comments found for sent params
+ *  HTTP/1.1 200 OK
+ *  []
+ *
+ */
 router.get('/v1/recent-comments', controllers.recentComments);
 router.get('/__gtg', controllers.gtg);
 router.get('/__about', controllers.about);
--- web.js
+++ web.js
@@ -1,19 +1,24 @@
 'use strict';
 
+const join = require('path').join;
 const express = require('express');
 const routes = require('./routes');
 const app = express();
 
 const server = require('http').Server(app);
 
+app.use(express.static(join(__dirname, '/static')));
 app.use('/', routes);
+app.get(['/', '/v1'], (req, res) => {
+	res.redirect('/apidoc');
+});
 /*eslint-disable no-unused-vars*/
 app.use((error, req, res, next) => {
-	res.status(500).send('Something went wrong');
+	res.status(500).end('Something went wrong');
 });
 /*eslint-enable no-unused-vars*/
 app.use('*', (req, res) => {
-	res.status(404).send('This endpoint does not exist');
+	res.status(404).end('This endpoint does not exist');
 });
 
 server.listen(process.env.PORT || 4000);
