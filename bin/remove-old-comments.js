5552470ae06482dabbcdeb66bb5927ce87a66cd6
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-01-04 14:16:18 +0200

    Replace raw query with sequelize destroy method

--- bin/remove-old-comments.js
+++ bin/remove-old-comments.js
@@ -5,12 +5,11 @@ const log = require('../services/logger');
 
 const OLDER_THAN = '30 days';
 
-const cleanupQuery = `DELETE FROM comments WHERE created_at < NOW() - INTERVAL '${OLDER_THAN}'`;
-
-models.sequelize.query(cleanupQuery, {
-	type: models.sequelize.QueryTypes.DELETE
-}).then(() => {
-	log.info({service: 'clean-old-comments'}, 'old comments deleted');
-}).catch((error) => {
-	log.error({service: 'clean-old-comments'}, error.message);
-});
+models.comment.destroy({
+	where: {
+		createdAt: {
+			lt: models.Sequelize.literal(`now() - '${OLDER_THAN}'::interval`)
+		}
+	}
+}).then(affectedRows => log.info({service: 'clean-old-comments'}, `${affectedRows} comments deleted`))
+	.catch(error => log.error({service: 'clean-old-comments'}, error.message));
